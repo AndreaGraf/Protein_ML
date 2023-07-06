349d420bb6bd85cdfa5893701d07e62f9e974d8a
AndreaGraf <andrea.grafmueller@gmail.com>
2023-07-06 09:04:34 +0200

    Deployed 4648af7 with MkDocs version: 1.4.3

--- protml/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Protein_MM model architectures and components
-"""
-
-from ..dataloaders import utils as data_utils
-from .baseline_vae import VAE
-from .baseline_supervised import ENC_M
-from .dz_supervised import ENC_M_dz
-
--- protml/models/components/__init__.py
+++
@@ -1,5 +0,0 @@
-""" Components of the model architecture. """
-from .loss_functions import GaussNLL_VAR_Loss, GaussNLLLoss
-from .downstream_models import Linear_Measurement
-from .downstream_models import Mave_Global_Epistasis_Measurement
-
